be1f096244acda0fdf9161af26bb810ab9fcadeb
Sandeep B <124546985+sandeepbegudem@users.noreply.github.com>
2024-02-11 23:28:13 -0600

    Update SecurityConfig.java

--- src/main/java/com/sandeepbegudem/customer/payments/service/config/SecurityConfig.java
+++ src/main/java/com/sandeepbegudem/customer/payments/service/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -50,15 +51,15 @@ public class SecurityConfig {
 
 
 // use this to skip BCryptPasswordEncoder
-    // @Bean
-    // public PasswordEncoder passwordEncoder() {
-    //     return NoOpPasswordEncoder.getInstance();
-    // }
+    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return NoOpPasswordEncoder.getInstance();
+//    }
 
-   @Bean
-   public PasswordEncoder passwordEncoder() {
-       return new BCryptPasswordEncoder();
-   }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public AuthenticationProvider authenticationProvider(){

bbde532fbafd4cacea8c3cfb42e62c6224d491f8
Sandeep B <124546985+sandeepbegudem@users.noreply.github.com>
2024-02-11 23:40:16 -0600

    fix: token issues (#30)
    fix: token issues

--- src/main/java/com/sandeepbegudem/customer/payments/service/controller/CustomerController.java
+++ src/main/java/com/sandeepbegudem/customer/payments/service/controller/CustomerController.java
@@ -3,9 +3,12 @@ package com.sandeepbegudem.customer.payments.service.controller;
 import com.sandeepbegudem.customer.payments.service.dto.CustomerPaymentsRequest;
 import com.sandeepbegudem.customer.payments.service.entity.Customer;
 import com.sandeepbegudem.customer.payments.service.service.CustomerService;
+import com.sandeepbegudem.customer.payments.service.service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,28 +17,47 @@ import java.util.List;
 @RequestMapping("/api/v1/customers")
 public class CustomerController {
 
-    private final CustomerService customerService;
+    private CustomerService customerService;
+
+    private JwtService jwtService;
+
+    private AuthenticationManager authenticationManager;
 
     @Autowired
-    public CustomerController(CustomerService customerService) {
+    public CustomerController(CustomerService customerService, JwtService jwtService, AuthenticationManager authenticationManager) {
         this.customerService = customerService;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
     }
 
     @GetMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<List<CustomerPaymentsRequest>> retrieveAllCustomers(){
 
         return new ResponseEntity<>(customerService.getAllCustomers(), HttpStatus.OK);
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('USER')")
     public ResponseEntity<Customer> insertCustomer(@RequestBody CustomerPaymentsRequest request){
 
         return new ResponseEntity<>(customerService.saveCustomer(request), HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<CustomerPaymentsRequest> retieveCustomerById(@PathVariable Integer id){
+    @PreAuthorize("hasAuthority('USER')")
+    public ResponseEntity<CustomerPaymentsRequest> retrieveCustomerById(@PathVariable Integer id){
 
         return new ResponseEntity<>(customerService.customerById(id), HttpStatus.OK);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deleteCustomerById(@PathVariable Integer id) {
+        if (id == null) {
+            throw new RuntimeException("id: " + " not found");
+        }
+        else
+            customerService.deleteCustomerById(id);
     }
 }
--- src/main/java/com/sandeepbegudem/customer/payments/service/service/CustomerService.java
+++ src/main/java/com/sandeepbegudem/customer/payments/service/service/CustomerService.java
@@ -1,1 +1,62 @@
-package com.sandeepbegudem.customer.payments.service.service;import com.sandeepbegudem.customer.payments.service.dto.CustomerPaymentsRequest;import com.sandeepbegudem.customer.payments.service.entity.Customer;import com.sandeepbegudem.customer.payments.service.mapper.CustomerMapper;import com.sandeepbegudem.customer.payments.service.repository.CustomerRepository;import org.springframework.beans.factory.annotation.Autowired;import org.springframework.stereotype.Service;import java.util.List;import java.util.stream.Collectors;@Servicepublic class CustomerService {    @Autowired    private CustomerRepository customerRepository;    @Autowired    private CustomerMapper mapper;    // retrieve all customers    public List<CustomerPaymentsRequest> getAllCustomers(){        List<CustomerPaymentsRequest> customerPaymentsRequests = customerRepository                .findAll()                .parallelStream()                .map(m -> mapper.entityToDto(m))                .collect(Collectors.toList());        return customerPaymentsRequests;    }    // save a customer    public Customer saveCustomer(CustomerPaymentsRequest customerPaymentsRequest) {        Customer customerList = mapper.dtoToEntity(customerPaymentsRequest);        Customer savedCustomer = customerRepository.save(customerList);        return savedCustomer;    }    // retrieve a customer by id    public CustomerPaymentsRequest customerById(Integer id){        Customer customer = customerRepository.findById(id).orElse(null);        CustomerPaymentsRequest customerPaymentsRequest = mapper.entityToDto(customer);        return customerPaymentsRequest;    }    // update customer    public Customer updateCustomer(CustomerPaymentsRequest customerPaymentsRequest, Integer id) {        Customer customer = mapper.dtoToEntity(customerPaymentsRequest);        Customer retrievedCustomer = customerRepository.findById(customer.getId()).orElse(null);        Customer savedCustomer = customerRepository.save(retrievedCustomer);        return savedCustomer;    }}
+package com.sandeepbegudem.customer.payments.service.service;
+import com.sandeepbegudem.customer.payments.service.dto.CustomerPaymentsRequest;
+import com.sandeepbegudem.customer.payments.service.entity.Customer;
+import com.sandeepbegudem.customer.payments.service.mapper.CustomerMapper;
+import com.sandeepbegudem.customer.payments.service.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CustomerMapper mapper;
+
+    // retrieve all customers
+    public List<CustomerPaymentsRequest> getAllCustomers(){
+        List<CustomerPaymentsRequest> customerPaymentsRequests = customerRepository
+                .findAll()
+                .parallelStream()
+                .map(m -> mapper.entityToDto(m))
+                .collect(Collectors.toList());
+        return customerPaymentsRequests;
+    }
+
+    // save a customer
+    public Customer saveCustomer(CustomerPaymentsRequest customerPaymentsRequest) {
+        Customer customerList = mapper.dtoToEntity(customerPaymentsRequest);
+        Customer savedCustomer = customerRepository.save(customerList);
+        return savedCustomer;
+    }
+
+    // retrieve a customer by id
+    public CustomerPaymentsRequest customerById(Integer id){
+        Customer customer = customerRepository.findById(id).orElse(null);
+        CustomerPaymentsRequest customerPaymentsRequest = mapper.entityToDto(customer);
+        return customerPaymentsRequest;
+    }
+
+    // update customer
+    public Customer updateCustomer(CustomerPaymentsRequest customerPaymentsRequest, Integer id) {
+        Customer customer = mapper.dtoToEntity(customerPaymentsRequest);
+        Customer retrievedCustomer = customerRepository.findById(customer.getId()).orElse(null);
+        Customer savedCustomer = customerRepository.save(retrievedCustomer);
+        return savedCustomer;
+    }
+
+    // delete customer
+    public void deleteCustomerById(int id){
+        Customer customer = customerRepository.findById(id).orElse(null);
+        if (customer.getId() == null){
+            throw new RuntimeException("resource: not found" + id);
+        }
+        else
+            customerRepository.deleteById(customer.getId());
+    }
+
+}

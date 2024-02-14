641dc9a51195cf3ce53f596e254951a970b4217a
Sandeep B <124546985+sandeepbegudem@users.noreply.github.com>
2024-02-13 23:24:30 -0600

    Minor tweaks (#36)

--- src/main/java/com/sandeepbegudem/customer/payments/service/controller/CustomerController.java
+++ src/main/java/com/sandeepbegudem/customer/payments/service/controller/CustomerController.java
@@ -1,1 +1,63 @@
-package com.sandeepbegudem.customer.payments.service.controller;import com.sandeepbegudem.customer.payments.service.dto.CustomerPaymentsRequest;import com.sandeepbegudem.customer.payments.service.entity.Customer;import com.sandeepbegudem.customer.payments.service.service.CustomerService;import org.springframework.beans.factory.annotation.Autowired;import org.springframework.http.HttpStatus;import org.springframework.http.ResponseEntity;import org.springframework.web.bind.annotation.*;import java.util.List;@RestController@RequestMapping("/api/v1/customers")public class CustomerController {    @Autowired    private CustomerService customerService;    @Autowired    public CustomerController(CustomerService customerService) {        this.customerService = customerService;    }    @GetMapping    public ResponseEntity<List<CustomerPaymentsRequest>> retrieveAllCustomers(){        return new ResponseEntity<>(customerService.getAllCustomers(), HttpStatus.OK);    }    @PostMapping    public ResponseEntity<Customer> insertCustomer(@RequestBody CustomerPaymentsRequest request){        return new ResponseEntity<>(customerService.saveCustomer(request), HttpStatus.CREATED);    }    @GetMapping("/{id}")    public ResponseEntity<CustomerPaymentsRequest> retieveCustomerById(@PathVariable Integer id){        return new ResponseEntity<>(customerService.customerById(id), HttpStatus.OK);    }}
+package com.sandeepbegudem.customer.payments.service.controller;
+
+import com.sandeepbegudem.customer.payments.service.dto.CustomerPaymentsRequest;
+import com.sandeepbegudem.customer.payments.service.entity.Customer;
+import com.sandeepbegudem.customer.payments.service.service.CustomerService;
+import com.sandeepbegudem.customer.payments.service.service.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/customers")
+public class CustomerController {
+
+    private CustomerService customerService;
+
+    private JwtService jwtService;
+
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    public CustomerController(CustomerService customerService, JwtService jwtService, AuthenticationManager authenticationManager) {
+        this.customerService = customerService;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @GetMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<List<CustomerPaymentsRequest>> retrieveAllCustomers(){
+
+        return new ResponseEntity<>(customerService.getAllCustomers(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('USER')")
+    public ResponseEntity<Customer> insertCustomer(@RequestBody CustomerPaymentsRequest request){
+
+        return new ResponseEntity<>(customerService.saveCustomer(request), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('USER')")
+    public ResponseEntity<CustomerPaymentsRequest> retrieveCustomerById(@PathVariable Integer id){
+
+        return new ResponseEntity<>(customerService.customerById(id), HttpStatus.OK);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deleteCustomerById(@PathVariable Integer id) {
+        if (id == null) {
+            throw new RuntimeException("id: " + " not found");
+        }
+        else
+            customerService.deleteCustomerById(id);
+    }
+}
--- src/test/java/com/sandeepbegudem/customer/payments/service/service/CustomerServiceTest.java
+++ src/test/java/com/sandeepbegudem/customer/payments/service/service/CustomerServiceTest.java
@@ -1,1 +1,214 @@
-package com.sandeepbegudem.customer.payments.service.service;import com.sandeepbegudem.customer.payments.service.dto.CustomerPaymentsRequest;import com.sandeepbegudem.customer.payments.service.entity.Customer;import com.sandeepbegudem.customer.payments.service.mapper.CustomerMapper;import com.sandeepbegudem.customer.payments.service.repository.CustomerRepository;import org.junit.jupiter.api.Assertions;import org.junit.jupiter.api.BeforeEach;import org.junit.jupiter.api.Test;import org.mockito.InjectMocks;import org.mockito.Mock;import org.mockito.Mockito;import org.mockito.MockitoAnnotations;import java.util.*;import java.util.function.Supplier;import static org.junit.jupiter.api.Assertions.*;import static org.mockito.ArgumentMatchers.any;import static org.mockito.Mockito.*;class CustomerServiceTest {    @InjectMocks    private CustomerService customerService;    @Mock    private CustomerMapper customerMapper;    @Mock    private CustomerRepository customerRepository;    @BeforeEach    void setUp() {        MockitoAnnotations.openMocks(this);    }    @Test    public void should_save_a_customer(){    // Given        CustomerPaymentsRequest request = new CustomerPaymentsRequest();        request.setId(1);        request.setFirstname("john");        request.setLastname("doe");        request.setAge(21);        request.setAddress("123 main st");        request.setCity("sanjose");        request.setState("california");        request.setPayments(null);        Customer customer = new Customer();        customer.setId(1);        customer.setFirstname("john");        customer.setLastname("doe");        customer.setAge(21);        customer.setAddress("123 main st");        customer.setCity("sanjose");        customer.setState("california");        customer.setPayments(null);        when(customerMapper.dtoToEntity(request)).thenReturn(customer);        when(customerRepository.save(customer)).thenReturn(customer);        // When        Customer customersDto = customerMapper.dtoToEntity(request);        Customer savedCustomer = customerRepository.save(customersDto);        // Then        assertEquals(request.getId(), savedCustomer.getId());        assertEquals(request.getFirstname(), savedCustomer.getFirstname());        assertEquals(request.getLastname(), savedCustomer.getLastname());        assertEquals(request.getAge(), savedCustomer.getAge());        assertEquals(request.getAddress(), savedCustomer.getAddress());        assertEquals(request.getCity(), savedCustomer.getCity());        assertEquals(request.getState(), savedCustomer.getState());        assertNull((Object) request.getPayments(), (Supplier<String>) savedCustomer.getPayments());    }    @Test    public void should_get_all_customers(){        // Given        List<Customer> cr = new ArrayList<>();        cr.add(new Customer(                1,                "john",                "doe",                21,                "123 main st",                "denver",                "colorado",                null));       CustomerPaymentsRequest crReq = new CustomerPaymentsRequest(               1,                "john",                "doe",                21,                "123 main st",                "denver",                "colorado",                null);        when(customerRepository.findAll()).thenReturn((List<Customer>) cr);        when(customerMapper.entityToDto(any(Customer.class)))                .thenReturn(new CustomerPaymentsRequest(1,                "john",                "smith",                21,                "123 main st",                "denver",                "california",                null));        // When        customerService.getAllCustomers();        // Then        assertEquals(cr.get(0).getId(), crReq.getId());        assertEquals(cr.get(0).getFirstname(), crReq.getFirstname());        assertEquals(cr.get(0).getLastname(), crReq.getLastname());        assertEquals(cr.get(0).getCity(), crReq.getCity());        assertEquals(cr.get(0).getState(), crReq.getState());    }    @Test    public void should_retrieve_a_customer_by_Id(){        Customer customer = new Customer();        customer.setId(1);        customer.setFirstname("john");        customer.setLastname("doe");        customer.setAge(21);        customer.setAddress("123 main st");        customer.setCity("denver");        customer.setState("colorado");        customer.setPayments(null);        // Mock the calls        when(customerRepository.findById(customer.getId()))                .thenReturn(Optional.of(customer));        when(customerMapper.entityToDto(any(Customer.class)))                .thenReturn(new CustomerPaymentsRequest(1,                        "john",                        "doe",                        21,                        "123 main st",                        "denver",                        "colorado",                        null));        CustomerPaymentsRequest custResponse = customerService.customerById(customer.getId());        assertEquals(customer.getId(), custResponse.getId());        assertEquals(customer.getFirstname(), custResponse.getFirstname());        assertEquals(customer.getLastname(), custResponse.getLastname());        assertEquals(customer.getAge(), custResponse.getAge());        assertEquals(customer.getAddress(), custResponse.getAddress());        assertEquals(customer.getCity(), custResponse.getCity());        assertEquals(customer.getState(), custResponse.getState());        Assertions.assertNull((Object) customer.getPayments(), (Supplier<String>) custResponse.getPayments());       // Assertions.assertNotNull((Object) customer.getPayments(), (Supplier<String>) custResponse.getPayments()); Fails as expected        verify(customerRepository, times(1)).findById(customer.getId());        verify(customerMapper, times(1)).entityToDto(customer);        verify(customerMapper, atLeastOnce());    }    // Todo: write testcases for}
+package com.sandeepbegudem.customer.payments.service.service;
+
+import com.sandeepbegudem.customer.payments.service.dto.CustomerPaymentsRequest;
+import com.sandeepbegudem.customer.payments.service.entity.Customer;
+import com.sandeepbegudem.customer.payments.service.mapper.CustomerMapper;
+import com.sandeepbegudem.customer.payments.service.repository.CustomerRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.*;
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class CustomerServiceTest {
+
+    @InjectMocks
+    private CustomerService customerService;
+    @Mock
+    private CustomerMapper customerMapper;
+    @Mock
+    private CustomerRepository customerRepository;
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void should_save_a_customer() throws Exception {
+    // Given
+
+        CustomerPaymentsRequest request = new CustomerPaymentsRequest(1,
+                "john",
+                "doe",
+                21,
+                "123 main st",
+                "sanjose",
+                "california",
+                null);
+
+        Customer customer = new Customer(1,
+                "john",
+                "doe",
+                21,
+                "123 main st",
+                "sanjose",
+                "california",
+                null);
+
+        when(customerMapper.dtoToEntity(request)).thenReturn(customer);
+        when(customerRepository.save(customer)).thenReturn(customer);
+
+        // When
+        Customer savedCustomer = customerService.saveCustomer(request);
+        CustomerPaymentsRequest customerRequest = customerMapper.entityToDto(savedCustomer);
+
+        Customer customerResponse = customerRepository.save(savedCustomer);
+
+        // Then
+        assertEquals(request.getId(), customerResponse.getId());
+        assertEquals(request.getFirstname(), customerResponse.getFirstname());
+        assertEquals(request.getLastname(), customerResponse.getLastname());
+        assertEquals(request.getAge(), customerResponse.getAge());
+        assertEquals(request.getAddress(), customerResponse.getAddress());
+        assertEquals(request.getCity(), customerResponse.getCity());
+        assertEquals(request.getState(), customerResponse.getState());
+       // assertNull((Object) request.getPayments(), (Supplier<String>) customerResponse.getPayments());
+
+    }
+
+    @Test
+    public void should_get_all_customers() throws Exception {
+
+        // Given
+        List<Customer> cr = new ArrayList<>();
+        cr.add(new Customer(
+                1,
+                "john",
+                "doe",
+                21,
+                "123 main st",
+                "denver",
+                "colorado",
+                null));
+
+       CustomerPaymentsRequest crReq = new CustomerPaymentsRequest(
+               1,
+                "john",
+                "doe",
+                21,
+                "123 main st",
+                "denver",
+                "colorado",
+                null);
+
+        when(customerRepository.findAll()).thenReturn((List<Customer>) cr);
+        when(customerMapper.entityToDto(any(Customer.class)))
+                .thenReturn(new CustomerPaymentsRequest(1,
+                "john",
+                "smith",
+                21,
+                "123 main st",
+                "denver",
+                "california",
+                null));
+
+        // When
+        customerService.getAllCustomers();
+
+        // Then
+        assertEquals(cr.get(0).getId(), crReq.getId());
+        assertEquals(cr.get(0).getFirstname(), crReq.getFirstname());
+        assertEquals(cr.get(0).getLastname(), crReq.getLastname());
+        assertEquals(cr.get(0).getCity(), crReq.getCity());
+        assertEquals(cr.get(0).getState(), crReq.getState());
+
+
+    }
+    @Test
+    public void should_retrieve_a_customer_by_Id() throws Exception {
+
+        Customer customer = new Customer(1,
+                "john",
+                "doe",
+                21,
+                "123 main st",
+                "denver",
+                "colorado",
+                null);
+
+
+        // Mock the calls
+        when(customerRepository.findById(customer.getId()))
+                .thenReturn(Optional.of(customer));
+        when(customerMapper.entityToDto(any(Customer.class)))
+                .thenReturn(new CustomerPaymentsRequest(1,
+                        "john",
+                        "doe",
+                        21,
+                        "123 main st",
+                        "denver",
+                        "colorado",
+                        null));
+
+        CustomerPaymentsRequest custResponse = customerService.customerById(customer.getId());
+
+        assertEquals(customer.getId(), custResponse.getId());
+        assertEquals(customer.getFirstname(), custResponse.getFirstname());
+        assertEquals(customer.getLastname(), custResponse.getLastname());
+        assertEquals(customer.getAge(), custResponse.getAge());
+        assertEquals(customer.getAddress(), custResponse.getAddress());
+        assertEquals(customer.getCity(), custResponse.getCity());
+        assertEquals(customer.getState(), custResponse.getState());
+        Assertions.assertNull((Object) customer.getPayments(), (Supplier<String>) custResponse.getPayments());
+       // Assertions.assertNotNull((Object) customer.getPayments(), (Supplier<String>) custResponse.getPayments()); Fails as expected
+
+        verify(customerRepository, times(1)).findById(customer.getId());
+        verify(customerMapper, times(1)).entityToDto(customer);
+        verify(customerMapper, atLeastOnce()).entityToDto(customer);
+    }
+
+    @Test
+    public void should_update_customer() throws Exception {
+
+        Integer customerId = 1;
+
+        CustomerPaymentsRequest request = new CustomerPaymentsRequest(1,
+                "john",
+                "doe",
+                21,
+                "123 main st",
+                "denver",
+                "colorado",
+                null);
+
+       Customer customer = new Customer(
+                1,
+                "john",
+                "doe",
+                21,
+                "123 main st",
+                "denver",
+                "colorado",
+                null);
+
+        when(customerRepository.findById(request.getId())).thenReturn(Optional.of(customer));
+        when(customerMapper.dtoToEntity(request)).thenReturn(customer);
+        when(customerRepository.save(customer)).thenReturn(new Customer(1,
+                "john",
+                "doe",
+                21,
+                "123 main st",
+                "denver",
+                "colorado",
+                null));
+
+        Customer updatedCustomer = customerService.updateCustomer(request, customer.getId());
+
+        assertEquals(request.getId(), updatedCustomer.getId());
+        assertEquals(customer.getFirstname(), updatedCustomer.getFirstname());
+        assertEquals(customer.getLastname(), updatedCustomer.getLastname());
+        assertEquals(customer.getAge(), updatedCustomer.getAge());
+        assertEquals(customer.getAddress(), updatedCustomer.getAddress());
+        assertEquals(customer.getCity(), updatedCustomer.getCity());
+        assertEquals(customer.getState(), updatedCustomer.getState());
+    }
+
+}
